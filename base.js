72fd9b3278f706752d8560ac275f8adbc8015120
jdln <jdln@weyland.(none)>
2013-12-14 15:57:56 -0600

    First commit.

---
+++ base.js
@@ -0,0 +1,25 @@
+if (Meteor.isClient) {
+  Template.email.events({
+    'click #btn': function () {
+      Meteor.call('sendEmail', $('#email').val());
+      Session.set('done', true);
+    }
+  });
+
+  Template.email.done = function () { 
+      if (Session.get('done')) return Session.equal('done', true);
+    }
+
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+
+  Meteor.methods({
+    sendEmail: function(email) {
+      Email.send({to:email, from:'test@mail.com', subject:'Thank you for signing up!', text: 'We will let you know when we have some news.'});
+    }
+  });
+}
